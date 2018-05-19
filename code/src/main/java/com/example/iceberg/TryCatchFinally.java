5414065d1d49aaa344944e7aa3d97ce429931e76
iceberg-jd <Dmitriy_Gulyaev@epam.com>
2018-05-19 20:50:21 +0400

    Corrected TryCatchFinally

--- code/src/main/java/com/example/iceberg/TryCatchFinally.java
+++ code/src/main/java/com/example/iceberg/TryCatchFinally.java
@@ -1,15 +1,12 @@
 package com.example.iceberg;
 
-import java.io.File;
-import java.io.IOException;
-
 public class TryCatchFinally {
 
-  void m(File file) throws IOException {
+  void m() {
     String str = "string";
     try {
       str.charAt(111);
-    } catch (Exception e) {
+    } catch (StringIndexOutOfBoundsException e) {
       e.getMessage();
     } finally {
       str.hashCode();

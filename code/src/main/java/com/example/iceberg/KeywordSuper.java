317c8b99eec4e9df54e7bf7f70f25f4fbaea15cb
iceberg-jd <Dmitriy_Gulyaev@epam.com>
2018-05-26 18:05:51 +0400

    Added MultiplicativeOperators and KeywordSuper

---
+++ code/src/main/java/com/example/iceberg/KeywordSuper.java
@@ -0,0 +1,10 @@
+package com.example.iceberg;
+
+public class KeywordSuper extends Thread{
+
+  @Override
+  public String toString() {
+    return super.toString();
+  }
+  
+}
---
+++ code/src/main/java/com/example/iceberg/MultiplicativeOperators.java
@@ -0,0 +1,18 @@
+package com.example.iceberg;
+
+public class MultiplicativeOperators {
+
+  int multiplicationInt(int m1, int m2) {
+    return m1 * m2;
+  }
+
+  int divisionInt(int m1, int m2) {
+    return m1 / m2;
+  }
+
+  int remainderInt(int m1, int m2) {
+    return m1 % m2;
+  }
+
+
+}

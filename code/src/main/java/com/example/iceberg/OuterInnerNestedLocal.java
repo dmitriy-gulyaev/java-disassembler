42d9b02d23e31c3bc26fc4c70338cddcfd931ef7
Dmitriy Gulyaev <dmitriy_gulyaev@git.epam.com>
2019-01-11 18:34:08 +0400

    OuterInnerNestedLocal.java modification

--- code/src/main/java/com/example/iceberg/OuterInnerNestedLocal.java
+++ code/src/main/java/com/example/iceberg/OuterInnerNestedLocal.java
@@ -1,29 +1,46 @@
 package com.example.iceberg;
 
-public class OuterInnerNestedLocal {
+class OuterInnerNestedLocal {
 
-    String fieldO = "";
+    Integer fieldOuter = new Integer(10000);
 
     class Inner {
-        void mI() {
-            fieldO.hashCode();
+        void methodInner() {
+            fieldOuter.hashCode();
         }
     }
 
     static class Nested {
+        void methodNested() {}
     }
 
 
-    void mO() {
+    void methodOuter() {
+        Integer fieldLocal = new Integer(80000);
+        final int fieldLocalFinal = 90000;
+
         class Local {
-            void mL() {
-                fieldO.hashCode();
+            void methodLocal() {}
+        }
+
+        class LocalWithAccesToOuter {
+            void methodLocalWithAccesToOuter() {
+                fieldLocal.hashCode();
+                new Integer(fieldLocalFinal);
             }
         }
-    }
 
+        new Inner().methodInner();
+        new Nested().methodNested();
+        new Local().methodLocal();
+        new LocalWithAccesToOuter().methodLocalWithAccesToOuter();
+
+    }
 }
 
-// Inner and local classes are similar to each other, with the exception of <tt>EnclosingMethod</tt>
-// attribute in local class. Both classes have single constructor with one parameter - reference to
-// outer class.
+// Inner and local classes are similar to each other, with the exception of <tt
+// class='keyword'>EnclosingMethod</tt> attribute in local class. Both classes have single
+// constructor with one parameter - reference to outer class. Reference is saved as 
+// synthetic instance variable <tt class='keyword'>this$0</tt> and may be used further for access to
+// outer class. Reference to outer class is passed as constructor parameter and saved even if
+// inner/local don't use it (according to source code).

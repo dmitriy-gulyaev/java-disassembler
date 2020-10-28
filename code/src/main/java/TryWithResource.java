fb5c8e93115608e1b3beca35ee7911ef77ed7edf
dmitriy_gulyaev <dmitriy_gulyaev@epam.com>
2020-10-28 22:31:48 +0400

    TryWithResources

---
+++ code/src/main/java/TryWithResource.java
@@ -0,0 +1,21 @@
+class TryWithResource {
+
+    void m1() throws Exception {
+        try (AutoCloseable a = newA()) {
+            a.hashCode();
+        }
+    }
+
+    void m2() {
+        try (AutoCloseable a = newA()) {
+            a.hashCode();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private AutoCloseable newA() {
+        return null;
+    }
+
+}
--- code/src/main/java/TryWithResources.java
+++ code/src/main/java/TryWithResources.java
@@ -1,20 +1,6 @@
 class TryWithResources {
 
-    void m1() throws Exception {
-        try (AutoCloseable a = newA()) {
-            a.hashCode();
-        }
-    }
-
-    void m2() {
-        try (AutoCloseable a = newA()) {
-            a.hashCode();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    void m3() throws Exception {
+    void m() throws Exception {
         try (
             AutoCloseable a1 = newA();
             AutoCloseable a2 = newA();
